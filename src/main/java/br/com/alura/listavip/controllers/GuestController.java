7b25bee49dc1762d63e3c90cfec78d18c5f96cab
fabricoliveira <fabricoliveira@192.168.0.101>
2019-05-30 02:03:24 -0300

    implementacao da lista de convidados

--- src/main/java/br/com/alura/listavip/controllers/GuestController.java
+++ src/main/java/br/com/alura/listavip/controllers/GuestController.java
@@ -1,10 +1,20 @@
 package br.com.alura.listavip.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.listavip.model.Guests;
+import br.com.alura.listavip.repository.GuestsRepository;
 
 @Controller
 public class GuestController {
+	
+	@Autowired
+	private GuestsRepository guestsRepository;
 	
 	@RequestMapping("/")
 	public String index() {
@@ -12,7 +22,11 @@ public class GuestController {
 	}
 	
 	@RequestMapping("viplist")
-	public String vipList() {
+	public String vipList(Model model) {
+		List<Guests> guests = guestsRepository.findAll();
+		
+		model.addAttribute("guests", guests);
+		
 		return "viplist";
 	}
 
---
+++ src/main/java/br/com/alura/listavip/model/Guests.java
@@ -0,0 +1,25 @@
+package br.com.alura.listavip.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity(name = "GUESTS")
+@Data
+public class Guests implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String telephone;
+
+}
