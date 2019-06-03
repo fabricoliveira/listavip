f1dca9ae88b92f1a1ef51bf02058dc77bf4cfd2b
fabricoliveira <fabricoliveira@192.168.0.101>
2019-06-02 21:11:14 -0300

    atualizacao nas configuracoes das classes e properties para melhor organizar.
    Plus atualizacao no pom para gerar o fatjar

--- src/main/java/br/com/alura/listavip/ListavipApplication.java
+++ src/main/java/br/com/alura/listavip/ListavipApplication.java
@@ -1,11 +1,7 @@
 package br.com.alura.listavip;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class ListavipApplication {
@@ -14,21 +10,4 @@ public class ListavipApplication {
 		SpringApplication.run(ListavipApplication.class, args);
 	}
 	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/viplist");
-		dataSource.setUsername("root");
-		dataSource.setPassword("novadata");
-		
-//		dataSource.setDriverClassName("org.postgresql.Driver");
-//		dataSource.setUrl("jdbc:postgresql://localhost:5432/viplist");
-//		dataSource.setUsername("root");
-//		dataSource.setPassword("root");
-		
-		
-		return dataSource;
-	}
-
 }
